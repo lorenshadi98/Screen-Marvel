c8aa31634701112c7157a081ed4332738c2cc805
lorenshadi98 <lorans.hadi@gmail.com>
2021-11-03 14:13:56 -0700

    Secret Key removed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 from sqlalchemy.exc import IntegrityError
 from flask import Flask, render_template, request, flash, redirect, session, g, send_from_directory, url_for
-from key import API_KEY
+from key import API_KEY, SECRET_KEY
 from models import connect_db, db, User, FavoriteMovie
 from forms import AddUserForm, EditUserForm, UserLoginForm
 CURR_USER_KEY = "curr_user"
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY)
 connect_db(app)
 
 
