ebe0c8a50016c42d9fd6adfa6a224162272d8c9f
lorenshadi98 <lorans.hadi@gmail.com>
2021-11-11 23:47:42 -0800

    Navbar active page UI update

--- static/index.js
+++ static/index.js
@@ -5,3 +5,12 @@ const navbarMenu = document.querySelector('#navbar-links');
 burgerIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('is-active');
 });
+
+$(function () {
+  $('a').each(function () {
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('is-active');
+      $(this).parents('li').addClass('is-active');
+    }
+  });
+});
